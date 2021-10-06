1eba60b6984c187f6aba209ca8d89e8d34d0a616
DESKTOP-0B9KPCU\Miki Itabashi <ita6.work@gmail.com>
2021-10-06 19:46:53 +0900

    トップページの制作実績作成

---
+++ src/js/top-works-swiper.js
@@ -0,0 +1,12 @@
+// トップページ制作実績用swiper
+var swiperTopWorks = new Swiper('.js-top-works-swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+
+    // If we need pagination
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true
+    },
+});
