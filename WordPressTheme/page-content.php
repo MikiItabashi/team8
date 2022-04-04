c2971f71bc0f653fbc6437fbe4b25520e2ff56ad
DESKTOP-0B9KPCU\Miki Itabashi <ita6.work@gmail.com>
2022-04-04 17:37:29 +0900

    事業内容ページ完了

--- WordPressTheme/page-content.php
+++ WordPressTheme/page-content.php
@@ -1,44 +1,39 @@
 <?php get_header(); ?>
 
-  <div class="l-sub-content">
-    <div class="p-sub-content">
-      <div class="p-sub-content__inner l-inner">
-        <div class="p-sub-content__head">
-          <p class="p-sub-content__title">企業理念</p>
-          <p class="p-sub-content__text">説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。</p>
-        </div>
-        <ul class="p-sub-content__items p-sub-content-list">
-          <li class="p-sub-content-list__item p-sub-content-item">
-            <div class="p-sub-content-item__img">
-              <img src="./images/content/sub-content1.jpg" alt="企業理念１">
-            </div>
-            <div class="p-sub-content-item__desc">
-              <p class="p-sub-content-item__title">企業理念１</p>
-              <p class="p-sub-content-item__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-            </div>
-          </li>
-          <li class="p-sub-content-list__item p-sub-content-item">
-            <div class="p-sub-content-item__img">
-              <img src="./images/content/sub-content2.jpg" alt="企業理念２">
-            </div>
-            <div class="p-sub-content-item__desc">
-              <p class="p-sub-content-item__title">企業理念２</p>
-              <p class="p-sub-content-item__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-            </div>
-          </li>
-          <li class="p-sub-content-list__item p-sub-content-item">
-            <div class="p-sub-content-item__img">
-              <img src="./images/content/sub-content3.jpg" alt="企業理念３">
-            </div>
-            <div class="p-sub-content-item__desc">
-              <p class="p-sub-content-item__title">企業理念３</p>
-              <p class="p-sub-content-item__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
-            </div>
-          </li>
-        </ul>
+<div class="l-sub-content">
+  <div class="p-sub-content">
+    <div class="p-sub-content__inner l-inner">
+      <div class="p-sub-content__head">
+        <p class="p-sub-content__title"><?php esc_html(the_field('content__top-title')); ?></p>
+        <p class="p-sub-content__text"><?php esc_html(the_field('content__top-text')); ?></p>
       </div>
+      <ul class="p-sub-content__items p-sub-content-list">
+
+        <!-- 繰り返しフィールド読み込み -->
+        <?php
+        $contentgroup = SCF::get('content__group');
+        foreach ($contentgroup as $fields) {
+          $imgurl = wp_get_attachment_image_src($fields['content__image'] , 'full');
+        ?>
+
+          <li class="p-sub-content-list__item p-sub-content-item">
+            <div class="p-sub-content-item__img">
+              <img src="<?php echo esc_url($imgurl[0]); ?>" alt="<?php echo esc_html($fields['content__title']); ?>">
+            </div>
+            <div class="p-sub-content-item__desc">
+              <p class="p-sub-content-item__title"><?php echo esc_html($fields['content__title']); ?></p>
+              <p class="p-sub-content-item__text"><?php echo esc_html($fields['content__text']); ?></p>
+            </div>
+          </li>
+        <?php } ?>
+
+
+
+
+      </ul>
     </div>
   </div>
+</div>
 
 <!-- お問い合わせセクション表示 -->
 <?php get_template_part('content-contact'); ?>
