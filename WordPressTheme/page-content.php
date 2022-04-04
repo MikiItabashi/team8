253a9bae7d36d1b16864bafeaf32d7a40f43e8d1
DESKTOP-0B9KPCU\Miki Itabashi <ita6.work@gmail.com>
2022-04-04 17:18:57 +0900

    事業内容ページ作成（編集中）

---
+++ WordPressTheme/page-content.php
@@ -0,0 +1,46 @@
+<?php get_header(); ?>
+
+  <div class="l-sub-content">
+    <div class="p-sub-content">
+      <div class="p-sub-content__inner l-inner">
+        <div class="p-sub-content__head">
+          <p class="p-sub-content__title">企業理念</p>
+          <p class="p-sub-content__text">説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。説明が入ります。</p>
+        </div>
+        <ul class="p-sub-content__items p-sub-content-list">
+          <li class="p-sub-content-list__item p-sub-content-item">
+            <div class="p-sub-content-item__img">
+              <img src="./images/content/sub-content1.jpg" alt="企業理念１">
+            </div>
+            <div class="p-sub-content-item__desc">
+              <p class="p-sub-content-item__title">企業理念１</p>
+              <p class="p-sub-content-item__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+            </div>
+          </li>
+          <li class="p-sub-content-list__item p-sub-content-item">
+            <div class="p-sub-content-item__img">
+              <img src="./images/content/sub-content2.jpg" alt="企業理念２">
+            </div>
+            <div class="p-sub-content-item__desc">
+              <p class="p-sub-content-item__title">企業理念２</p>
+              <p class="p-sub-content-item__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+            </div>
+          </li>
+          <li class="p-sub-content-list__item p-sub-content-item">
+            <div class="p-sub-content-item__img">
+              <img src="./images/content/sub-content3.jpg" alt="企業理念３">
+            </div>
+            <div class="p-sub-content-item__desc">
+              <p class="p-sub-content-item__title">企業理念３</p>
+              <p class="p-sub-content-item__text">テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。テキストが入ります。</p>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+<!-- お問い合わせセクション表示 -->
+<?php get_template_part('content-contact'); ?>
+
+<?php get_footer(); ?>
