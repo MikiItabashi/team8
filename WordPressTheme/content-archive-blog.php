f804aeb9896514b7dacbae4c39010ef887ef18e0
tanabe <arisecolor@gmail.com>
2022-04-19 03:59:04 +0900

    archive-blog-2022/4/18-commit

---
+++ WordPressTheme/content-archive-blog.php
@@ -0,0 +1,20 @@
+<!-- ブログカード出力 -->
+<?php
+$terms = get_the_terms($post->ID, 'custom_category'); //ターム一覧抽出
+?>
+
+<div class="p-sub-blog__card">
+  <a href="<?php the_permalink(); ?>" class="p-blog-card">
+    <figure class="p-blog-card__img">
+      <?php echo get_card_image("blog"); ?>
+    </figure>
+    <div class="p-blog-card__body">
+      <p class="p-blog-card__title"><?php the_title(); ?></p>
+      <p class="p-blog-card__text"><?php echo get_the_excerpt(); ?></p>
+      <div class="p-blog-card__info">
+        <p class="p-blog-card__category"><?php echo esc_html($terms[0]->name); ?></p>
+        <time class="p-blog-card__date" datetime="<?php the_time('Y-m-d') ?>">"<?php the_time('Y.m.d') ?></time>
+      </div>
+    </div>
+  </a>
+</div>
---
+++ WordPressTheme/taxonomy-custom_category.php
@@ -0,0 +1,60 @@
+<?php get_header(); ?>
+<!-- カテゴリリスト表示 -->
+<div class="l-category-list">
+  <div class="c-category-list">
+    <ul class="c-category-list__items l-inner">
+
+      <li class="c-category-list__item">
+        <a class="c-category-list__link" href="<?php echo esc_url(home_url('blog')); ?>">
+          <span>ALL</span>
+        </a>
+      </li>
+      <?php
+      $terms = get_terms('custom_category');
+      if ($terms) {
+        foreach ($terms as $term) :
+          include 'content-category.php';
+        endforeach;
+      }
+      ?>
+    </ul>
+  </div>
+</div>
+
+<div class="l-sub-blog">
+  <div class="p-sub-blog">
+    <div class="p-sub-blog__inner l-inner">
+      <div class="p-sub-blog__body">
+        <?php
+        $terms_obj = get_queried_object(); //現在のタクソノミー情報取得
+        $terms_slug = $terms_obj->slug; //現在のタクソノミーslug取得
+        $paged = get_query_var('paged') ? get_query_var('paged') : 1;
+        $args = array(
+          'post_type' => 'blog',
+          'post_status' => 'publish',
+          'posts_per_page' => 9,
+          'paged' => $paged,
+          'tax_query' => array(
+            array(
+              'taxonomy' => 'custom_category',
+              'field' => 'slug',
+              'terms' => $terms_slug,
+            ),
+          ),
+        );
+        $the_query = new WP_Query($args);
+        if ($the_query->have_posts()) :
+          while ($the_query->have_posts()) : $the_query->the_post();
+            include 'content-archive-blog.php';
+          endwhile;
+        endif;
+        ?>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- お問い合わせセクション表示 -->
+<?php get_template_part('content-contact'); ?>
+
+<?php get_footer(); ?>
